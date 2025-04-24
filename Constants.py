2e521f0248317849d704c7ed993a727f76486205
Mike Sterry <msterry@users.noreply.github.com>
2025-04-24 10:25:50 -0500

    initial

---
+++ Constants.py
@@ -0,0 +1,36 @@
+CUSTOM_FORGE_BASE_URL = 'https://ignition4.customsforge.com/?search'
+CUSTOM_FORGE_SEARCH_KEY = '%5Bvalue%5D'
+CUSTOM_FORGE_LENGTH_KEY = 'length'
+CUSTOM_FORGE_LENGTH_VALUE = '500'
+CUSTOM_FORGE_ARRANGEMENT_KEY = 'filter_arrangement%5B%5D'
+CUSTOM_FORGE_REQUESTED_COLUMNS = 'draw=6&columns%5B0%5D%5Bdata%5D=addBtn&columns%5B0%5D%5Bsearchable%5D=false&columns%5B0%5D%5Borderable%5D=false&' + \
+        'columns%5B1%5D%5Bdata%5D=artistName&columns%5B1%5D%5Bname%5D=artist.name&columns%5B2%5D%5Bdata%5D=titleName&' + \
+        'columns%5B2%5D%5Bname%5D=title&columns%5B3%5D%5Bdata%5D=albumName&columns%5B3%5D%5Bname%5D=album&' + \
+        'columns%5B4%5D%5Bdata%5D=year&columns%5B5%5D%5Bdata%5D=duration&columns%5B5%5D%5Borderable%5D=false&' + \
+        'columns%5B6%5D%5Bdata%5D=tunings&columns%5B6%5D%5Bsearchable%5D=false&columns%5B6%5D%5Borderable%5D=false&' + \
+        'columns%5B7%5D%5Bdata%5D=version&columns%5B7%5D%5Bsearchable%5D=false&columns%5B7%5D%5Borderable%5D=false&' + \
+        'columns%5B8%5D%5Bdata%5D=memberName&columns%5B8%5D%5Bname%5D=author.name&columns%5B9%5D%5Bdata%5D=created_at&' + \
+        'columns%5B9%5D%5Bsearchable%5D=false&columns%5B10%5D%5Bdata%5D=updated_at&columns%5B10%5D%5Bsearchable%5D=false&' + \
+        'columns%5B11%5D%5Bdata%5D=downloads&columns%5B11%5D%5Bsearchable%5D=false&columns%5B12%5D%5Bdata%5D=parts&' + \
+        'columns%5B12%5D%5Borderable%5D=false&columns%5B13%5D%5Bdata%5D=platforms&columns%5B14%5D%5Bdata%5D=file_pc_link&' + \
+        'columns%5B14%5D%5Bsearchable%5D=false&columns%5B15%5D%5Bdata%5D=file_mac_link&columns%5B15%5D%5Bsearchable%5D=false&' + \
+        'columns%5B16%5D%5Bdata%5D=artist.name&columns%5B17%5D%5Bdata%5D=title&columns%5B18%5D%5Bdata%5D=album&' + \
+        'columns%5B19%5D%5Bdata%5D=author.name&columns%5B20%5D%5Bdata%5D=discussionID&order%5B0%5D%5Bcolumn%5D=11&' + \
+        'order%5B0%5D%5Bdir%5D=desc&start=0&table_filter_artist=&table_filter_album=&filter_title=&filter_album=&' + \
+        'filter_preferred_platform=pc&filter_start_year=&filter_end_year=&filter_preferred=&filter_lovedCreators=&' + \
+        'filter_official=&filter_rsplus=&filter_disable=&filter_notdisabled=&filter_hidden=&_=1673467590856'
+
+REQUEST_HEADER_USER_AGENT = 'User-Agent'
+REQUEST_HEADER_USER_AGENT_VALUE = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:128.0) Gecko/20100101 Firefox/128.0'
+REQUEST_HEADER_ACCEPT = 'Accept'
+REQUEST_HEADER_ACCEPT_VALUE = 'application/json, text/javascript, */*; q=0.01'
+REQUEST_HEADER_ACCEPT_LANGUAGE = 'Accept-Language'
+REQUEST_HEADER_ACCEPT_LANGUAGE_VALUE = 'en-US,en;q=0.5'
+REQUEST_HEADER_REFERER = 'Referer'
+REQUEST_HEADER_REFERER_VALUE = 'https://ignition4.customsforge.com/'
+REQUEST_HEADER_CSRF_TOKEN = 'X-CSRF-TOKEN'
+REQUEST_HEADER_X_REQUESTED_WITH = 'X-Requested-With'
+REQUEST_HEADER_X_REQUESTED_WITH_VALUE = 'XMLHttpRequest'
+REQUEST_HEADER_CONNECTION = 'Connection'
+REQUEST_HEADER_CONNECTION_VALUE = 'keep-alive'
+REQUEST_HEADER_COOKIE = 'Cookie'
---
+++ README.md
@@ -0,0 +1,12 @@
+### Rocksmith CustomForge Downloader
+
+A simple link grabber for Rocksmith CustomForge, a community-driven platform for custom songs in the game Rocksmith.
+
+### Features
+- Download links custom songs from CustomForge for a given artist
+
+### Usage
+- Run the script and enter the artist name
+- The script will fetch and display the download links for all songs by that artist available on CustomForge
+- You can then use these links to download the songs directly
+- The script requires some session info from a CustomForge session, which can be obtained by logging in to the site and copying the session cookie from your browser
